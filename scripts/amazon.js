c2b64d63d00f6f88e90ff7bd5e83556da4fc78e1
yvngsimba <52266231+yvngsimba@users.noreply.github.com>
2023-09-13 22:03:18 -0400

    Adding products throught javav script

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -22,6 +22,32 @@ const products = [{
     count: 56
   },
   priceCents: 799
+}, {
+  image: 'images/products/black-2-slot-toaster.jpg',
+  name: '2 Slot Toaster - Black',
+  rating: {
+    stars: 5,
+    count: 2197
+  },
+  priceCents: 1899
+}, {
+  image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name: '6 Piece White Dinner Plate Set',
+  rating: {
+    stars: 4,
+    count: 37
+  },
+  priceCents: 2067
+}, {
+  image: 'images/products/6-piece-non-stick-baking-set.webp',
+  name: '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set',
+  rating: {
+  stars: 4.5 ,
+  count: 175
+  },
+  priceCents: 3499
+}, {
+  image: 'images/products/plain-hooded-fleece-sweatshirt-yellow.jpg',
 }];
 
 let productsHTML = ''
