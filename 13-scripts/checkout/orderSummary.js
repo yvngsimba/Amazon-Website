a6809828e5c8838882ee499be197b085ab1ccdaa
yvngsimba <52266231+yvngsimba@users.noreply.github.com>
2023-11-04 18:24:40 -0400

    Make payment summary interactive

--- 13-scripts/checkout/orderSummary.js
+++ 13-scripts/checkout/orderSummary.js
@@ -4,13 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDelivery} from'../../13-javascript-amazon-project-main copy/data/deliveryOptions.js';
-
-hello();
-
-
-const today = dayjs();
-const deliveryDate = today.add(7,'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -138,7 +132,7 @@ export function renderOrderSummary() {
       );
       container.remove();
 
-      updateCartQuantity()
+      renderPaymentSummary();
     });
   });
 
@@ -178,7 +172,8 @@ export function renderOrderSummary() {
 
         const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
         updateCartQuantity();
-        renderOrderSummary(); 
+        renderOrderSummary();
+        renderPaymentSummary();
       });
   });
 
@@ -188,6 +183,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
